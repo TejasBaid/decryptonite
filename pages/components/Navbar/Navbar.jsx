c430b4fb4cc247713644b3a807fee138ad24fb8a
parav-jaikrishan <r21947parav@dpsrkp.net>
2022-09-04 21:52:11 +0530

    Feat: Minor update

--- pages/components/Navbar/Navbar.jsx
+++ pages/components/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Navbar() {
     return (
-        <nav className="flex h-20 w-screen fixed">
-            <h1 className="text-white">Hello</h1>
+        <nav className="flex h-20 w-screen justify-between items-center fixed z-1">
+            <div className="flex ml-16">
+                <a className="text-white" href="/">Home</a>
+            </div>
         </nav>
     );
 }
