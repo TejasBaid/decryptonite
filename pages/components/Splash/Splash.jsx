975004c28c7f5c22be807a7fff1ced1abb86525f
parav-jaikrishan <r21947parav@dpsrkp.net>
2022-09-04 23:57:30 +0530

    Feat: letter-spacing added and margin increased

--- pages/components/Splash/Splash.jsx
+++ pages/components/Splash/Splash.jsx
@@ -5,9 +5,9 @@ export default function Splash() {
             <h1 className="strokedText mt-5 lg:mt-8 text-4xl lg:text-5xl font-bold text-transparent">Decryptonite</h1>
             <h1 className="text-4xl lg:text-5xl text-white font-bold mt-5 lg:mt-8">2022-23</h1>
             <div className="buttons-container flex w-screen justify-center flex-col lg:flex-row mt-6">
-                <a className="mr-0 lg:mr-6 mb-6 lg:mb-0" href="https://www.discord.com">Discord</a>
-                <a className="mr-0 lg:mr-6 mb-6 lg:mb-0" href="/register">Register</a>
-                <a className="mr-0 lg:mr-6 mb-6 lg:mb-0" href="https://docs.google.com/document/d/1bpeRtE2YC09lHyKIuPUFJkxxp1gfuPPvRneCMJxPv7U/edit">Resources</a>
+                <a className="mr-0 lg:mr-10 mb-6 lg:mb-0" href="https://www.discord.com">Discord</a>
+                <a className="mr-0 lg:mr-10 mb-6 lg:mb-0" href="/register">Register</a>
+                <a className="mr-0 lg:mr-10 mb-6 lg:mb-0" href="https://docs.google.com/document/d/1bpeRtE2YC09lHyKIuPUFJkxxp1gfuPPvRneCMJxPv7U/edit">Resources</a>
             </div>
         </div>
     );
