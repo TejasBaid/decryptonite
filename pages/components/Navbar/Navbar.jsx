b3ef6f996d391289e8a77f84a92ff2ac3c97774d
parav-jaikrishan <r21947parav@dpsrkp.net>
2022-09-04 22:48:50 +0530

    Feat: Created navbar

--- pages/components/Navbar/Navbar.jsx
+++ pages/components/Navbar/Navbar.jsx
@@ -1,10 +1,20 @@
-import Link from "next/link";
-
 export default function Navbar() {
     return (
-        <nav className="flex h-20 w-screen justify-between items-center fixed z-1">
-            <div className="flex ml-16">
-                <a className="text-white" href="/">Home</a>
+        <nav className="flex flex-wrap-reverse lg:flex-nowrap h-16 lg:h-20 w-screen justify-between items-center fixed z-1">
+            <div className="links hidden lg:flex">
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Register</a>
+                <a href="/">Login</a>
+            </div>
+            <div className="images ml-8 lg:mr-16 h-2/3 flex">
+                <img className="mr-4" src="/exun-logo.png" alt="Exun logo"/>
+                <img src="/decryptonite.png" alt="Decryptonite logo"/>
+            </div>
+            <div className="hamburger cursor-pointer block lg:hidden absolute right-8">
+                <span className="bar"/>
+                <span className="bar"/>
+                <span className="bar"/>
             </div>
         </nav>
     );
