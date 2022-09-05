752f5041821d0fce9691db5f417203eaee429eab
parav-jaikrishan <r21947parav@dpsrkp.net>
2022-09-05 22:49:32 +0530

    Feat: Redesign

--- pages/components/Splash/Splash.jsx
+++ pages/components/Splash/Splash.jsx
@@ -3,7 +3,7 @@ export default function Splash() {
         <div className="splash w-screen h-screen flex flex-col items-center justify-center">
             <img className="w-1/2 lg:w-1/6 rounded-md" src="/decryptonite.png"/>
             <h1 className="strokedText mt-5 lg:mt-8 text-4xl lg:text-5xl font-bold text-transparent">Decryptonite</h1>
-            <h1 className="text-4xl lg:text-5xl text-white font-bold mt-5 lg:mt-8">2022-23</h1>
+            <h1 className="text-3xl lg:text-5xl text-white font-bold mt-5 lg:mt-8">2022-23</h1>
             <div className="buttons-container flex w-screen justify-center flex-col items-center lg:flex-row mt-6">
                 <a className="mr-0 lg:mr-10 mb-6 lg:mb-0" href="https://www.discord.com">Discord</a>
                 <a className="mr-0 lg:mr-10 mb-6 lg:mb-0" href="/register">Register</a>
